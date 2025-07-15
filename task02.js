fdd84305883d23e5735c1d239d31d4a032566532
Peera <peerawit.sut@gmail.com>
2025-07-15 11:58:05 +0700

    feat: sent my task01-task02

--- task02.js
+++ task02.js
@@ -5,3 +5,9 @@
 
 const userInput = "42";
 // Try changing to "hello", 10, null, NaN
+
+if (typeof(userInput) === "number" && !isNaN(userInput) ) {
+    console.log("yes it's a number");
+} else {
+    console.log("no it's not a number");
+}
